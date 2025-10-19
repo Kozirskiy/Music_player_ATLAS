4e2d8f76a77a7e157b381b8fc7e1a5cd444d70b1
Atlas <atlasmail@atlas.com>
2025-10-19 13:44:42 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.module.css'
+import s from './App.module.css'
+
+export const App =()=> {
+
+  return (
+    <>
+        <div className={s.mainContainer}>
+            <h1 className={s.titleMainContainer}>Music player</h1>
+        </div>
+    </>
+  )
+}
